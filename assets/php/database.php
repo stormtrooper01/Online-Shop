524c0160c2c8868afec3240ee18b6f4cc74e06b8
Yelena <lena.skripnik.01@gmail.com>
2020-06-05 18:13:34 +0300

    Update database.php

--- assets/php/database.php
+++ assets/php/database.php
@@ -4,4 +4,4 @@ $databaseName = 'internet_shop_db';
 $userName = 'root';
 $password = ' ';
 
-$link = mysqli_connect($hostName, $userName, $password, $databaseName) or die("Cannot connect to server: " . mysqli_error($link));
+$link = mysqli_connect($hostName, $userName, $password, $databaseName);
